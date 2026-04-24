4a4df97d6ca67e8aff516db64418c193592bfc9d
roberdea1h-blip <roberdea1h@gmail.com>
2026-04-23 23:53:36 -0600

    feat(user): add user repository

--- src/main/java/com/multi/tent/pos_api/user/UserRepository.java
+++ src/main/java/com/multi/tent/pos_api/user/UserRepository.java
@@ -1,5 +1,20 @@
 package com.multi.tent.pos_api.user;
 
-public class UserRepository {
-    
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    Optional<User> findByJoinCode(String joinCode);
+
+    List<User> findByActiveTrue();
 }
